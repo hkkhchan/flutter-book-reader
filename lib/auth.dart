afe7634572958f1d961fd52101d719ddbe5aa502
hkkhchan <wkkhchan@gmail.com>
2019-09-04 21:13:16 +0800

    reg

--- lib/auth.dart
+++ lib/auth.dart
@@ -9,13 +9,13 @@ abstract class BaseAuth{
 
 class Auth implements BaseAuth{
   Future<String> signInWithEmailAndPassword(String email, String password) async{
-    FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-    return user.uid; 
+    AuthResult authResult = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+    return authResult.user.uid;
   }
 
   Future<String> createUserWithEmailAndPassword(String email, String password) async {
-    FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-    return user.uid;
+    AuthResult authResult = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+    return authResult.user.uid;
   }
 
   Future<String> fetchSignInMethodsForEmail(String email) async {
